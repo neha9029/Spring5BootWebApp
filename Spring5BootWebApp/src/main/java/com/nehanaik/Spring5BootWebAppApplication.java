63063960ee4541f5212ad52a5cee974c06642a28
neha9029 <nehanaik9029@gmail.com>
2018-05-18 14:28:27 -0400

    Added Spring boot starters and dependencies JPA, H2 database, Thymeleaf template using Spring Initialzr project

---
+++ Spring5BootWebApp/src/main/java/com/nehanaik/Spring5BootWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.nehanaik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5BootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5BootWebAppApplication.class, args);
+	}
+}
